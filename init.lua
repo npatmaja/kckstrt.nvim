ae93eee4267247edd2c5655dac269387ff4fd7cc
nauval <nauval.atmaja@gmail.com>
2025-07-31 23:59:52 +0700

    feat: install mbbill/undotree for better undo navigation

--- init.lua
+++ init.lua
@@ -1082,14 +1082,21 @@ require('lazy').setup({
     opts = {},
   },
 
+  -- Ziglang
+  'ziglang/zig.vim',
+
+  { -- Better undo
+    'mbbill/undotree',
+    keys = {
+      { '<leader>uu', vim.cmd.UndotreeToggle, desc = 'Toggle Undotree side bar' },
+    }
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
   require 'kickstart.plugins.autoformat',
   require 'kickstart.plugins.debug',
-
-  -- Ziglang
-  'ziglang/zig.vim',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
